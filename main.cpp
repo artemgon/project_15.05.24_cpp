b0936ccf684eb222d9eae373e6c58410a236584c
artemgon <artemhoncharuk123@gmail.com>
2024-05-15 20:01:09 +0300

    Creating the second task.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+	const int size = 5;
+	int array[size];
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = i + 1;
+	}
+	for (int i = size - 1; i > -1; i--)
+	{
+		cout << array[i] << " ";
+	}
 	return 0;
 }

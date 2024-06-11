8804af5cf8b3fbc0e949ec3413bc521e1622e91d
artemgon <artemhoncharuk123@gmail.com>
2024-06-11 22:18:13 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,19 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-	const int SIZE = 8;
-	int array[SIZE];
-	for (int i = 0; i < SIZE; i++){
-		array[i] = rand() % 600 - 300;
+	const int size = 5;
+	int array[size], min, max, i = 0;
+	for (; i < size; i++){
+		array[i] = rand() % 20 - 10;
 		cout << array[i] << " ";
 	}
-	int min, max;
 	min = max = array[0];
-	for (int i = 0; i < SIZE; i++) {
+	for (i = 1; i < size; i++) {
 		if (array[i] < min)
 			min = array[i];
 		if (array[i] > max)
 			max = array[i];
 	}
-	cout << "\nMin: " << min << endl; 
-	cout << "Max: " << max << endl;
+	cout << "\nMin: " << min << "\nMax: " << max;
 	return 0;
 }

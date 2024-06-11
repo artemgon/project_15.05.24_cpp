3ed46db2e303b625c22f9cf67f4933283d594cbd
artemgon <artemhoncharuk123@gmail.com>
2024-06-11 21:27:09 +0300

    Modifying the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,31 +2,14 @@
 using namespace std;
 
 int main() {
-	int first_month;
-	cout << "Please, enter the amount of money earned during the first month:";
-	cin >> first_month;
-	int second_month;
-	cout << "Please, enter the amount of money earned during the second month:";
-	cin >> second_month;
-	int third_month;
-	cout << "Please, enter the amount of money earned during the third month:";
-	cin >> third_month;
-	int fourth_month;
-	cout << "Please, enter the amount of money earned during the fourth month:";
-	cin >> fourth_month;
-	int fifth_month;
-	cout << "Please, enter the amount of money earned during the fifth month:";
-	cin >> fifth_month;
-	int sixth_month;
-	cout << "Please, enter the amount of money earned during the sixth month:";
-	cin >> sixth_month;
-	int income[6] = {first_month, second_month, third_month, fourth_month, fifth_month, sixth_month};
-	int sum = 0;
-	int size = 6;
+	const int size = 6;
+	int profit[size], sum = 0;
 	for (int i = 0; i < size; i++)
 	{
-		sum = sum + income[i];
+		cout << "Please, enter the profit in the " << i + 1 << "th month: ";
+		cin >> profit[i];
+		sum += profit[i];
 	}
-	cout << "The income is: " << sum;
+	cout << "The profit is: " << sum;
 	return 0;
 }

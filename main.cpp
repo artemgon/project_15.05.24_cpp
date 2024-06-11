624dc44b94fd9748aecf08184a0ae3a21867a553
artemgon <artemhoncharuk123@gmail.com>
2024-06-11 21:53:02 +0300

    Modifying the third task of homework.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,14 @@
 using namespace std;
 
 int main() {
-	cout << "Please, enter the sizes of pentagon sides:";
-	int side_1;
-	cin >> side_1;
-	int side_2;
-	cin >> side_2;
-	int side_3;
-	cin >> side_3;
-	int side_4;
-	cin >> side_4;
-	int side_5;
-	cin >> side_5;
-	int array[5] = { side_1, side_2, side_3, side_4, side_5 };
-	int perimeter = 0;
-	for (int i = 0; i < 5; i++)
+	const int size = 5;
+	int sides[size], perimeter = 0;
+	cout << "Please, enter the sides of pentagon: ";
+	for (int i = 0; i < size; i++)
 	{
-		perimeter = perimeter + array[i];
+		cin >> sides[i];
+		perimeter += sides[i];
 	}
-	cout << "The perimeter is " << perimeter;
+	cout << "The perimeter is: " << perimeter;
 	return 0;
 }
